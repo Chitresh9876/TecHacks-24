55343350c8ffc6aa9e2ed408b69761f7d2cb168c
Chitresh9876 <chitreshgupta9876@gmail.com>
2024-01-20 03:11:26 +0530

    home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,33 +2,47 @@ import React from "react";
 import "./Home.css";
 import HeroImage from "../../images/HeroImage.jpg";
 import ImageDescription from "../../components/ImageDescription/ImageDescription";
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-// import Geolocation from "../../Api/GeoLoactionApi";
+import { Button } from "@mui/material";
+import Footer from "../../components/Footer/Footer";
+
 const Home = () => {
   
 
   return (
     <div>
+      {/* Hero Section */}
+
       <div className="heroSection">
-        <div className="heroSectionDetail"></div>
+        <div className="heroSectionDetail">
+          <h1>Welcome to Quality Checker</h1>
+          <h3>
+            We care about your heath,
+            <br /> Join with us to know about your surrounding.
+          </h3>
+          <Button
+            variant="outlined"
+            sx={{
+              backgroundColor: "#ffffff",
+              borderColor: "#ffffff",
+              ":hover": { color: "blue" },
+            }}
+          >
+            Sign Up
+          </Button>
+        </div>
         <img src={HeroImage} alt="" className="HeroImage" />
       </div>
       <div className="right-incline"></div>
       <div className="left-incline"></div>
 
-      <ImageDescription
-        text={
-          " Lorem or sit amet consectetur adipisicing elit. Explicabo nihil magni dolorem illo dolore odit sit minus illum voluptatibus est, eaque sequi esse repellat nemo in laudantium doloribus reiciendis? Sit?z ipsum dolor sit amet consectetur adipisicing elit. Explicabo nihil magni dolorem illo dolore odit sit minus illum voluptatibus est, eaque sequi esse repellat nemo in laudantium doloribus reiciendis? Sit?z"
-        }
-      />
-      <ImageDescription
-        position={"right"}
-        text={
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo nihil magni dolorem illo dolore odit sit minus illum voluptatibus est, eaque sequi esse repellat nemo in laudantium doloribus reiciendis? Sit?z"
-        }
-      />
-    </div>
+      {/* Body*/}
+
+      <ImageDescription />
+
+      {/* Footer */}
+
+      <Footer />
+ </div>
   );
 };
 
