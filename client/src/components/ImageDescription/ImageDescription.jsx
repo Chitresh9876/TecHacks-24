7ccf975d0f333f451c265d430aa1294f80659548
Chitresh9876 <chitreshgupta9876@gmail.com>
2024-01-19 23:58:04 +0530

    home page

--- client/src/components/ImageDescription/ImageDescription.jsx
+++ client/src/components/ImageDescription/ImageDescription.jsx
@@ -1,17 +1,70 @@
-import React from 'react'
-import styles from'./ImageDescription.module.css'
-import IMG1 from '../../images/Image2.jpg'
-import IMG2 from '../../images/Image1.jpg'
+import React from "react";
+import styles from "./ImageDescription.module.css";
+import IMG1 from "../../images/WQImage.jpg";
+import IMG2 from "../../images/AQImage.jpg";
+import { Stack } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
 
-const ImageDescription = ({position, text}) => {
+const ImageDescription = () => {
   return (
-    <div className={position==='right'? styles.containerRightImage : styles.containerLeftImage}>
-      <div className={styles.image}>
-        <img src={position==='right'? IMG1 : IMG2} alt="" />
-      </div>
-      <div className={styles.description}>{text}</div>
-    </div>
-  )
-}
+    <div>
+      <Stack direction="row" justifyContent="space-evenly" mt={5} alignItems={"center"}> 
+        <div className={styles.image}>
+          <img src={IMG1} alt="air image" />
+        </div>
+        <Card sx={{ maxWidth: 600, height: "20rem", boxShadow:"none"}} mt={5}>
+          <CardActionArea>
+            <CardContent>
+              {/* <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography> */}
+              <Typography variant="body2" color="text.secondary" fontSize={20} fontWeight={500}>
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button color="primary">
+              Reade More...
+            </Button>
+          </CardActions>
+        </Card>
+      </Stack>
 
-export default ImageDescription
+      <Stack direction="row-reverse" justifyContent="space-evenly" mt={5} alignItems={"center"}> 
+        <div className={styles.image}>
+          <img src={IMG2} alt="air image" />
+        </div>
+        <Card sx={{ maxWidth: 600, height: "20rem", boxShadow:"none"}} mt={5}>
+          <CardActionArea>
+            <CardContent>
+              {/* <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography> */}
+              <Typography variant="body2" color="text.secondary" fontSize={20} fontWeight={500}>
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button color="primary">
+              Reade More...
+            </Button>
+          </CardActions>
+        </Card>
+      </Stack>
+    </div>
+  );
+};
+
+export default ImageDescription;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,40 +1,47 @@
-import React from 'react'
-import './Home.css'
-import HeroImage from '../../images/HeroImage.jpg'
-import ImageDescription from '../../components/ImageDescription/ImageDescription'
-import Footer from '../../components/Footer/Footer'
+import React from "react";
+import "./Home.css";
+import HeroImage from "../../images/HeroImage.jpg";
+import ImageDescription from "../../components/ImageDescription/ImageDescription";
+import { Button } from "@mui/material";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
     <div>
+      {/* Hero Section */}
 
-                {/* Hero Section */}
-
-        <div className="heroSection">
-            <div className="heroSectionDetail">fff</div>
-            <img src={HeroImage} alt="" className='HeroImage' />
+      <div className="heroSection">
+        <div className="heroSectionDetail">
+          <h1>Welcome to Quality Checker</h1>
+          <h3>
+            We care about your heath,
+            <br /> Join with us to know about your surrounding.
+          </h3>
+          <Button
+            variant="outlined"
+            sx={{
+              backgroundColor: "#ffffff",
+              borderColor: "#ffffff",
+              ":hover": { color: "blue" },
+            }}
+          >
+            Sign Up
+          </Button>
         </div>
-        <div className="right-incline"></div>
-        <div className="left-incline"></div>
+        <img src={HeroImage} alt="" className="HeroImage" />
+      </div>
+      <div className="right-incline"></div>
+      <div className="left-incline"></div>
 
-                {/* Body */}
+      {/* Body */}
 
-        <ImageDescription
-        text={' Lorem or sit amet consectetur adipisicing elit. Explicabo nihil magni dolorem illo dolore odit sit minus illum voluptatibus est, eaque sequi esse repellat nemo in laudantium doloribus reiciendis? Sit?z ipsum dolor sit amet consectetur adipisicing elit. Explicabo nihil magni dolorem illo dolore odit sit minus illum voluptatibus est, eaque sequi esse repellat nemo in laudantium doloribus reiciendis? Sit?z'}
-        />
-        <ImageDescription
-        position={'right'}
-        text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo nihil magni dolorem illo dolore odit sit minus illum voluptatibus est, eaque sequi esse repellat nemo in laudantium doloribus reiciendis? Sit?z'}
-        />
+      <ImageDescription />
 
-             {/* Footer */}
-      <Footer/>
+      {/* Footer */}
 
-
+      <Footer />
     </div>
+  );
+};
 
-            
-  )
-}
-
-export default Home
+export default Home;
